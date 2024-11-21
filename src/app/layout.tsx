932e20bf3eb84a3d505997218e84d5c083f97ca0
KeanuWhoa <ryan@hedgecowebsites.com>
2024-11-21 16:21:03 -0700

    Cleanup
    Removed footer links as they aren't needed. Added year function for footer.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Metadata } from "next";
-import { Anton, Poppins } from 'next/font/google';
+import { Anton, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/(ui)/header/Header";
 import Footer from "@/app/(ui)/footer/Footer";
 
 const anton = Anton({
     weight: "400",
-    variable: '--ff-anton',
-    subsets: ['latin']
+    variable: "--ff-anton",
+    subsets: ["latin"],
 });
 
 const poppins = Poppins({
     weight: ["400", "500"],
-    variable: '--ff-poppins',
-    subsets: ['latin']
-})
+    variable: "--ff-poppins",
+    subsets: ["latin"],
+});
+
+const date: any = new Date(Date.now()).toLocaleDateString(undefined, {
+    year: "numeric",
+});
 
 export const metadata: Metadata = {
-  title: "DJ EPX",
-  description: "Over a 13+ year career, DJ EPX regularly performs at iconic nightclubs in multiple cities, mid-major festivals, and has garnered support and performed with countless tier 1 dance acts including Tiësto, David Guetta, Martin Garrix, Alesso, Steve Aoki, Afrojack, and many more.",
+    title: "DJ EPX",
+    description:
+        "Over a 13+ year career, DJ EPX regularly performs at iconic nightclubs in multiple cities, mid-major festivals, and has garnered support and performed with countless tier 1 dance acts including Tiësto, David Guetta, Martin Garrix, Alesso, Steve Aoki, Afrojack, and many more.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${anton.variable} ${poppins.variable}`}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${anton.variable} ${poppins.variable}`}>
+                <Header />
+                {children}
+                <Footer date={date} />
+            </body>
+        </html>
+    );
 }
